b03fd432a83dfce1d66e15c2429e63dc19487bf4
Neon Zhou <791745910@qq.com>
2022-02-04 14:32:50 +0800

    完成int过滤结果概率直方统计

--- src/java/simpledb/optimizer/IntHistogram.java
+++ src/java/simpledb/optimizer/IntHistogram.java
@@ -6,6 +6,12 @@ import simpledb.execution.Predicate;
  */
 public class IntHistogram {
 
+    private int buckets;
+    private int min;
+    private int max;
+    private int width;
+    private int ntups;
+    private int[] histogram;
     /**
      * Create a new IntHistogram.
      * 
@@ -24,6 +30,13 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+        this.buckets = buckets;
+        this.min = min;
+        this.max = max;
+        double range = (double) (max - min + 1) / buckets;
+        width = (int) Math.ceil(range);
+        this.ntups = 0;
+        this.histogram = new int[buckets];
     }
 
     /**
@@ -32,6 +45,18 @@ public class IntHistogram {
      */
     public void addValue(int v) {
     	// some code goes here
+        int idx = valueToIdx(v);
+        histogram[idx]++;
+        ntups++;
+    }
+
+    // 通过v的值，锁定在哪个桶里
+    private int valueToIdx(int v) {
+         if(v == max) {
+             return buckets - 1;
+         } else {
+             return (v - min) / width;
+         }
     }
 
     /**
@@ -46,9 +71,74 @@ public class IntHistogram {
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
 
-    	// some code goes here
-        return -1.0;
+        // some code goes here
+        int bucketIdx = valueToIdx(v);
+        int height = 0; // height为直方图高度，即落在这个桶的数据数量
+        int right = bucketIdx * width + min + width - 1; // 桶的右边界索引
+        int left = bucketIdx * width + min; // 桶的左边界索引
+
+        switch (op) {
+            case EQUALS:
+                if (v < min || v > max) {
+                    return 0.0;
+                } else {
+                    height = histogram[bucketIdx];
+                    return (double) height / width / ntups;
+                }
+
+            case GREATER_THAN:
+                if (v < min) {
+                    return 1.0;
+                } else if (v > max) {
+                    return 0.0;
+                } else {
+                    height = histogram[bucketIdx];
+                    // p1: 这个桶范围内大于v的概率
+                    double p1 = ((right - v) / width * 1.0) * (height * 1.0 / ntups);
+                    int allAtRight = 0;
+                    for (int i = bucketIdx + 1; i < buckets; i++) {
+                        allAtRight += histogram[i];
+                    }
+                    // p2: 所有这个桶右侧的数据出现概率
+                    double p2 = allAtRight * 1.0 / ntups;
+                    return p1 + p2;
+                }
+
+            case LESS_THAN:
+                if (v < min) {
+                    return 0.0;
+                } else if (v > max) {
+                    return 1.0;
+                } else {
+                    height = histogram[bucketIdx];
+                    double p1 = ((v - left) / width * 1.0) * (height * 1.0 / ntups);
+                    int allAtLeft = 0;
+                    for (int i = bucketIdx - 1; i >= 0; i--) {
+                        allAtLeft += histogram[i];
+                    }
+                    double p2 = allAtLeft * 1.0 / ntups;
+                    return p1 + p2;
+                }
+
+            case LESS_THAN_OR_EQ:
+                return estimateSelectivity(Predicate.Op.EQUALS, v) + estimateSelectivity(Predicate.Op.LESS_THAN, v);
+
+            case GREATER_THAN_OR_EQ:
+                return estimateSelectivity(Predicate.Op.EQUALS, v) + estimateSelectivity(Predicate.Op.GREATER_THAN, v);
+
+            case LIKE:
+                //String才支持like
+                return avgSelectivity();
+
+            case NOT_EQUALS:
+                return 1 - estimateSelectivity(Predicate.Op.EQUALS, v);
+
+            default:
+                throw new RuntimeException("Op错误");
+        }
     }
+
+
     
     /**
      * @return
@@ -58,8 +148,7 @@ public class IntHistogram {
      *     join optimization. It may be needed if you want to
      *     implement a more efficient optimization
      * */
-    public double avgSelectivity()
-    {
+    public double avgSelectivity(){
         // some code goes here
         return 1.0;
     }
